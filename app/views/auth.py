5f06a43630e5c2188484fd46903ae4ca18b7f8f7
kyakulumbye ahmad <kyakusahmed@outlook.com>
2019-03-27 16:07:38 -0700

    [Delivers #164889250] add create user account endpoint

--- app/views/auth.py
+++ app/views/auth.py
@@ -17,3 +17,31 @@ def index():
     return jsonify({
         "status": 200, 'message': 'welcome to Epic Email.'
         }), 200
+
+
+@app.route('/api/v1/auth/signup', methods=['POST'])
+@swag_from('../docs/signup.yml')
+def create_user_account():
+    """add new user to self.users"""
+    data = request.get_json()
+    val_input_data = validator.input_data_validation([
+        'email', 'firstname', 'lastname', 'password'
+        ])
+    if val_input_data:
+        return jsonify({"status": 400, "error": val_input_data}), 400
+
+    registered = user.get_user_by_email(data['email'])
+    if registered:
+        return jsonify({
+            "status": 200, "message": "user registered already"
+            }), 200
+    user_info = user.create_user_account(
+        data['firstname'],
+        data['lastname'],
+        data['email'],
+        data['password']
+    )
+    new_user = [{
+        "user": user_info
+        }]
+    return jsonify({"data": new_user, "status": 201}), 201
