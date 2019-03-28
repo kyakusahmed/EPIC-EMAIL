e5a74746cddba453f4a27f9eb91c215e944301fc
kyakulumbye ahmad <kyakusahmed@outlook.com>
2019-03-27 22:45:51 -0700

    [Delivers #164888014] add create group endpoint

--- app/views/groups.py
+++ app/views/groups.py
@@ -11,3 +11,29 @@ group = Group()
 messages = Messages()
 user = User()
 validator = Validation()
+
+
+@app.route('/api/v1/groups', methods=['POST'])
+@jwt_required
+@swag_from('../docs/send_emails_to_individuals.yml')
+def create_group():
+    """send email to an individual."""
+    current_user = get_jwt_identity()
+    if current_user[6] != "user":
+        return jsonify({"message": "unauthorized access"})
+    validate_info = validator.input_data_validation([
+        'group_name', 'user_role'
+        ])
+    if validate_info:
+        return jsonify({
+            "message": 'Validation error',
+            "errors": validate_credentials
+        }), 400
+    data = request.get_json()
+    send_group = group.add_group(
+        current_user[0],
+        data['group_name'],
+        data['user_role'],
+        )
+    return jsonify({
+        "data": [{"message": send_group}], "status": 201}), 201
