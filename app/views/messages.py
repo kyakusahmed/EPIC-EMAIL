fc406b37881d40012375af921760f4070614668f
kyakulumbye ahmad <kyakusahmed@outlook.com>
2019-03-27 21:59:28 -0700

    [Delivers #164945452] add delete_users_message_from_inbox endpoint

--- app/views/messages.py
+++ app/views/messages.py
@@ -10,3 +10,19 @@ from flask_jwt_extended import (jwt_required, get_jwt_identity)
 messages = Messages()
 user = User()
 validator = Validation()
+
+
+@app.route('/api/v1/messages/delete/<int:message_id>', methods=['DELETE'])
+@jwt_required
+@swag_from('../docs/delete_user_inbox_email.yml')
+def delete_user_message(message_id):
+    """delete user's message"""
+    current_user = get_jwt_identity()
+    if current_user[6] != "user":
+        return jsonify({"status": 401, "message": "unauthorized access"})
+    search_message = messages.search_message(message_id)
+    if not search_message:
+        return jsonify({"status": 404, "messaege": "message not found"}), 404
+    return jsonify({
+        "message": messages.delete_message(message_id), "status": 200
+        }), 200
